9efb6cff5270fb2e34ea5533db8d542f26c97bf4
nnoeglez <>
2026-01-26 13:59:16 -0800

    Added IDataSource and changed InMemeoryDataSource

---
+++ src/main/java/edu/ucsd/spendingtracker/datasource/IDataSource.java
@@ -0,0 +1,12 @@
+package edu.ucsd.spendingtracker.datasource;
+
+import java.util.List;
+import edu.ucsd.spendingtracker.model.Expense;
+
+public interface IDataSource {
+    List<Expense> getExpenses();
+
+    void addExpense(Expense expense);
+
+    void deleteExpense(int id);
+}
--- src/main/java/edu/ucsd/spendingtracker/model/Expense.java
+++ src/main/java/edu/ucsd/spendingtracker/model/Expense.java
@@ -4,11 +4,20 @@ public class Expense {
     private String name;
     private Category category;
     private double amount;
+    private int id;
+
+    public Expense(String name, Category category, double amount, int id) {
+        this.name = name;
+        this.category = category;
+        this.amount = amount;
+        this.id = id;
+    }
 
     public Expense(String name, Category category, double amount) {
         this.name = name;
         this.category = category;
         this.amount = amount;
+        this.id = -1;
     }
 
     public String getName() {
@@ -21,6 +30,10 @@ public class Expense {
 
     public double getAmount() {
         return amount;
+    }
+
+    public int getId(){
+        return id;
     }
 }
 
