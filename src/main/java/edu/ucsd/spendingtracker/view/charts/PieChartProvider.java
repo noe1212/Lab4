613ed90812332ea14b707471ca3927c04e17e440
nnoeglez <>
2026-02-02 15:24:50 -0800

    done

---
+++ src/main/java/edu/ucsd/spendingtracker/view/charts/PieChartProvider.java
@@ -0,0 +1,52 @@
+package edu.ucsd.spendingtracker.view.charts;
+
+import java.util.Map;
+import edu.ucsd.spendingtracker.model.Category;
+import javafx.collections.FXCollections;
+import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.PieChart;
+
+
+public class PieChartProvider  implements IChartProvider{
+    @Override
+    public Node createChart(Map<Category, Double> data){
+        ObservableList<PieChart.Data> pieData = FXCollections.observableArrayList();
+
+        for(Map.Entry<Category, Double> entry: data.entrySet()){
+            Category  category = entry.getKey();
+            double total;
+            if(entry.getValue() == null){
+                total = 0.0;
+            }
+            else{
+                total = entry.getValue();
+            }
+            pieData.add(new PieChart.Data(category.toString(), total));
+
+        }
+        PieChart chart = new PieChart(pieData);
+
+        for(PieChart.Data slice: chart.getData()){
+            String color = Category.valueOf(slice.getName()).color;
+            Node node = slice.getNode();
+            if(node != null){
+                node.setStyle("-fx-pie-color: " + color + ";");
+            }
+        }
+        chart.setLegendVisible(false);
+
+        return chart; 
+    }
+
+    @Override
+    public String getDisplayName(){
+        return "Pie Chart";
+    }
+    
+}
